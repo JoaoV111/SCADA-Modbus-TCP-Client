b1b64d4204ec525c944c46a8f27c63ae0e3b3f50
joaov111 <joaocesar93@hotmail.com>
2019-10-10 14:47:25 -0300

    heroku update

--- thermomix/settings.py
+++ thermomix/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3',
-        'CONN_MAX_AGE': 500),
+        'CONN_MAX_AGE': 500,
     }
 }
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
