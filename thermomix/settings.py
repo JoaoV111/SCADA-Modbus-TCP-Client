3d813cdd2ac2e5d53413a9b9a1602cac4c6eb462
joaov111 <joaocesar93@hotmail.com>
2019-10-10 14:52:49 -0300

    heroku update

--- thermomix/settings.py
+++ thermomix/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'thermomix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'CONN_MAX_AGE': 500,
     }
 }
